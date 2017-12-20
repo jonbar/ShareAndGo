f26261171ea1e27597bab128d005b142fa223dbf
barrutieta <barrutieta@192.168.32.101>
2017-12-20 08:58:33 +0100

    primer push hecho y derecho

---
+++ vista/multipagina/header.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>                        
+            </button>
+            <img src="../img/logo.png" style="height: 100px"/><br>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Registrate</a></li>
+                <li><a href="#">Publicar viaje</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Mi cuenta</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
