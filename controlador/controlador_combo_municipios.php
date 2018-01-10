e5a30bcb5e011bf576909aea2b8c42b1d0a7e38a
barrutieta <barrutieta@192.168.32.101>
2018-01-10 13:00:37 +0100

    Revert "-COMBOBOX en proceso pero conseguido ;)"
    This reverts commit 08213a00d5fa0def46c2d2fc746b8522ca25dafb.

--- controlador/controlador_combo_municipios.php
+++
@@ -1,8 +0,0 @@
-<?php 
-require_once '../modelo/modelo_municipios.php';
-$id = 16;
-$cont=new modelo_municipios();
-$datos=$cont->cargar_municipios($id);
-
-$municipios = json_encode($datos);
-print $municipios;  
